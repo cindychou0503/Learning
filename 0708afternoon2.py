69c5a35f88248c627c9bca9bf8c83098ae1b0667
cindychou0503 <136140266+cindychou0503@users.noreply.github.com>
2023-07-08 06:42:31 +0000

    modify

--- 0708afternoon2.py
+++ 0708afternoon2.py
@@ -2,6 +2,19 @@
 
 import random
 min = 1
-max = 100
+max = 10
+count = 0
 target = random.randint(min, max)
-print(target)
+print("=======猜數字遊戲=======\n\n")
+
+while True:
+    keyin = int(input(f"猜數字範圍{min}~{max}:"))
+    count += 1
+    if(keyin == target):
+        print(f"Bingo！猜對了，答案是:{target}")
+        print(f"您共猜了幾{count}次")
+        break
+    else:
+        print("猜錯了")
+        print(f"您已經猜了幾{count}次")
+print("遊戲結束")
